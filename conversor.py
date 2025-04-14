705a18dc69aab862e85ae74622874d0609535d99
Sofs2 <sofschafranski@gmail.com>
2025-04-14 20:53:36 -0300

    funções de conversão add

---
+++ conversor.py
@@ -0,0 +1,11 @@
+def celsius_para_fahrenheit(celsius):
+    return celsius * 9/5 + 32
+
+def celsius_para_kelvin(celsius):
+    return celsius + 273.15
+
+def fahrenheit_para_celsius(fahrenheit):
+    return (fahrenheit - 32) * 5/9
+
+def fahrenheit_para_kelvin(fahrenheit):
+    return (fahrenheit + 459.67) * 5/9
